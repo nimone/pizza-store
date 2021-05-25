555ddc6d4fc2c10da48c443a37d08b8a0de5f7dd
nimone <nishantmogha1234@gmail.com>
2021-05-25 12:02:43 +0530

    add Global Styles and use Kanit fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { GlobalStyle } from './GlobalStyles'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <GlobalStyle />
         <Navbar />
       </Router>
     </div>
