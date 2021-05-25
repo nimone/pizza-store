4dc0fafcc04750a1aa13b829f50e2406692cefe1
nimone <nishantmogha1234@gmail.com>
2021-05-25 11:52:35 +0530

    add Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Nav, NavLink, NavIcon, Bars } from './NavbarElements'
+
+function Navbar() {
+	return (
+		<>
+			<Nav>
+				<NavLink to='/'>Pizza</NavLink>
+				<NavIcon>
+					<p>Menu</p>
+					<Bars />
+				</NavIcon>
+			</Nav>
+		</>
+	)
+}
+export default Navbar
