a63ddedde512ae45a5d0250f8a03d11ae3d411a1
nimone <nishantmogha1234@gmail.com>
2021-05-25 11:06:21 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Pizza Store</h1>
+    </div>
+  )
+}
+
+export default App
