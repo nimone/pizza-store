147dde9954166ad5251aa08aaaea9bfc6dbfa9cb
nimone <nishantmogha1234@gmail.com>
2021-05-27 11:56:09 +0530

    Add deserts section with 3 deserts listings

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { GlobalStyle } from './GlobalStyles'
 import Hero from './components/Hero'
 import Products from './components/Products'
-import { productsData } from './components/Products/data'
+import productsData from './components/Products/data'
+import Featured from './components/Featured'
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <Router>
         <GlobalStyle />
         <Hero />
-        <Products heading="Choose Your Favourite" data={productsData} />
+        <Products heading="Choose Your Favourite" data={productsData.pizzas} />
+        <Featured />
+        <Products heading="Sweet Treats for You" data={productsData.deserts} />
       </Router>
     </div>
   )
